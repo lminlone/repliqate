b967076c48b81704fe62b0f38fdca50c0fb130f1
terence <terence@lminl.one>
2025-12-16 00:37:54 +0000

    Integrated a CLI parser for the cli client

--- Repliqate/RepliqateClientCli.cs
+++ Repliqate/RepliqateClientCli.cs
@@ -1,3 +1,4 @@
+using System.CommandLine;
 using GrpcDotNetNamedPipes;
 using RepliqateProtos;
 
@@ -5,26 +6,61 @@ namespace Repliqate;
 
 public class RepliqateClientCli
 {
+    private IpcService.IpcServiceClient? _client = null;
+    
     public void Start(string[] args)
     {
+        RootCommand rootCommand = new("Repliqate CLI");
+
+        Command cmdVersion = new("version", "Display Repliqate version");
+
+        Option<string> optBackupContainer = new("--container")
+        {
+            Description = "The name of the container to backup"
+        };
+        Option<bool> optBackupNow = new("--now")
+        {
+            Description = "Backup now, bypassing schedule"
+        };
+        Command cmdBackup = new("backup", "Schedule a backup manually")
+        {
+            optBackupContainer,
+            optBackupNow
+        };
+        cmdBackup.SetAction(pr => CmdBackup(
+            pr.GetValue(optBackupNow),
+            pr.GetValue(optBackupContainer)
+        ));
+
+        // Add the sub commands to the main root command
+        rootCommand.Subcommands.Add(cmdVersion);
+        rootCommand.Subcommands.Add(cmdBackup);
+        
+        ParseResult parseResult = rootCommand.Parse(args);
+        
         var channel = new NamedPipeChannel(".", "repliqate");
-        var client = new IpcService.IpcServiceClient(channel);
+        _client = new IpcService.IpcServiceClient(channel);
         
         // Ensure connection, be nice to the server
-        ConnectConfirmReply? confirmReply = client.ConnectConfirm(new ConnectConfirmRequest());
+        ConnectConfirmReply? confirmReply = _client.ConnectConfirm(new ConnectConfirmRequest());
         if (confirmReply == null)
         {
             Console.WriteLine("Failed to connect to IPC server");
             return;
         }
 
+        parseResult.Invoke();
+        
+        _client.DisconnectConfirm(new DisconnectConfirmRequest());
+    }
+
+    private void CmdBackup(bool now, string? containerName = "")
+    {
         // Repliqate version
-        VersionReply? repliqateVersion = client.GetVersion(new VersionRequest());
-        DockerVersionReply? dockerVersion = client.GetDockerVersion(new DockerVersionRequest());
+        VersionReply? repliqateVersion = _client.GetVersion(new VersionRequest());
+        DockerVersionReply? dockerVersion = _client.GetDockerVersion(new DockerVersionRequest());
         
         Console.WriteLine($"Repliqate version: {repliqateVersion.Version}");
         Console.WriteLine($"Docker version: {dockerVersion.Version}");
-        
-        client.DisconnectConfirm(new DisconnectConfirmRequest());
     }
 }
